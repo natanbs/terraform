76dd090d4c2d0eedf552c475c8d7aa47627d2521
Natan Bensoussan <natanb@tikalk.com>
2020-02-05 16:46:54 +0200

    s3 env

--- main/backend.tf
+++ main/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "cx-terraform-state"
     region         = "eu-central-1"
     dynamodb_table = "NatanTerraformStatelock_test"
-    key            = "main/terraform_test.tfstate"
+    key            = "main/terraform${var.env}.tfstate"
   }
 }
